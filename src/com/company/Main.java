6d1411500c62fa3438fc9dac0a3c15ed8d04f3a9
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-03-29 16:27:21 +0430

    mili meter to cant meter

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m= sc.nextInt();
+        System.out.println(mmToCc(m));
+    }
+
+    public static int mmToCc(int m) {
+       m=m*1000;
+        return m;
+    }
+}
